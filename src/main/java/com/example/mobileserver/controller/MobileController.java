7ed3cd622d78f38c3a275b3f17580d71da1b2838
Đinh Mạnh Hải <haidm@thinhtientech.com>
2022-05-25 21:04:23 +0700

    entity

--- src/main/java/com/example/mobileserver/controller/MobileController.java
+++ src/main/java/com/example/mobileserver/controller/MobileController.java
@@ -3,6 +3,7 @@ package com.example.mobileserver.controller;
 import com.example.mobileserver.config.BaseMethodResponse;
 import com.example.mobileserver.config.Constants;
 import com.example.mobileserver.config.GetMethodResponse;
+import com.example.mobileserver.config.OperationNotImplementException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -18,16 +19,22 @@ public class MobileController {
   private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
       .getLogger(MobileController.class);
 
-  @GetMapping(path = "/signup-ctv")
+  @GetMapping(path = "/signup")
   public ResponseEntity<?> signupCTV(HttpServletRequest request) {
     try {
 //      Integer userId = authGuard.getUserId(request);
 //      dto.setUserId(userId);
-//      ResponseCallApiHRTech response = v3MService.createUserCTV(dto);
+      ResponseCallApiHRTech response = v3MService.createUserCTV(dto);
       return new ResponseEntity<>(
           GetMethodResponse.builder().status(true).data(true)
               .message(Constants.SUCCESS_MSG)
               .errorCode(HttpStatus.OK.name().toLowerCase()).httpCode(HttpStatus.OK.value()).build()
+          , HttpStatus.OK);
+    } catch (OperationNotImplementException e) {
+      logger.error(e.getMessage());
+      return new ResponseEntity<>(
+          BaseMethodResponse.builder().status(false).message(e.getMessage())
+              .errorCode(e.getMessageCode()).httpCode(HttpStatus.BAD_REQUEST.value()).build()
           , HttpStatus.OK);
     } catch (Exception e) {
       logger.error(e.getMessage());
