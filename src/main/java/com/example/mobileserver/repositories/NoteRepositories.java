1b9022c49aa7053a4b7e914d62463fa954e6bae1
Đinh Mạnh Hải <haidm@thinhtientech.com>
2022-05-28 06:38:08 +0700

    create, delete, sua not

--- src/main/java/com/example/mobileserver/repositories/NoteRepositories.java
+++ src/main/java/com/example/mobileserver/repositories/NoteRepositories.java
@@ -1,10 +1,16 @@
 package com.example.mobileserver.repositories;
 
 import com.example.mobileserver.entities.NoteUser;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoteRepositories extends JpaRepository<NoteUser, Integer> {
 
+  @Query("select a from NoteUser a where a.userId = ?1")
+  List<NoteUser> findByUserId(String userId);
+
+  NoteUser findOneById(Integer id);
 }
